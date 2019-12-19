d7f0260b9fb6510d076548a0b03b5f17e76657ad
Daniel Castiblanco <razzziel001@gmail.com>
2019-12-18 22:08:18 -0500

    Prueba de web simple con Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./compartido/header/header.component";
+import { FooterComponent } from "./compartido/footer/footer.component";
+import { PrincipalComponent } from "./secciones/principal/principal.component";
+import { DestacadosComponent } from "./secciones/destacados/destacados.component";
+import { ViviendasComponent } from "./secciones/viviendas/viviendas.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PrincipalComponent,
+    DestacadosComponent,
+    ViviendasComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
